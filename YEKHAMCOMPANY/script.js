0512f1eba106c46a91973a8ad2cbab8b8c1536c4
yekham <yektahamit@gmail.com>
2023-02-01 19:29:28 +0300

    Company Landing Page
    Web programlama dersi için oluşturduğum web sitesi.

---
+++ YEKHAMCOMPANY/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 600) {
+      $('#back-to-top').fadeIn();
+    } else {
+      $('#back-to-top').fadeOut();
+    }
+  });
+
+  $('#back-to-top').click(function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 400);
+    return false;
+  });
+});
+
